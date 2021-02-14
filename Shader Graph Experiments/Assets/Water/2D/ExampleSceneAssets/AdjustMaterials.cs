f47f48687ac3c8d1e50e4939e7967b565b3c758c
serj <13497925+gamedevserj@users.noreply.github.com>
2021-02-14 16:15:50 +0300

    Fixed 2d water reflection issue

---
+++ Shader Graph Experiments/Assets/Water/2D/ExampleSceneAssets/AdjustMaterials.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustMaterials : MonoBehaviour
+{
+
+    public Camera cam;
+    public Renderer[] waterRenderers;
+
+    Transform[] transforms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transforms = new Transform[waterRenderers.Length];
+        for (int i = 0; i < transforms.Length; i++)
+        {
+            transforms[i] = waterRenderers[i].GetComponent<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < waterRenderers.Length; i++)
+        {
+            waterRenderers[i].material.SetFloat("_ObjectPositionY", transforms[i].position.y);
+            waterRenderers[i].material.SetFloat("_CameraOrthoSize", cam.orthographicSize);
+        }
+    }
+}
